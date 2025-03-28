a5c367c6dd49917d086ca27acc66e51c87ade81c
wichitoSP <99522188+wichitoSP@users.noreply.github.com>
2025-03-27 21:34:16 -0600

    Se crea Modelo Mascota al proyecto

---
+++ src/main/java/org/example/Model/ModelMascota.java
@@ -0,0 +1,14 @@
+package org.example.Model;
+
+import java.util.Date;
+
+public class ModelMascota {
+    private int id;
+    private  String nombre;
+    private String especie;
+    private String raza;
+    private Date fechaNacimiento;
+    private double peso;
+    private char sexo;
+    
+}

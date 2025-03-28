e597216ca2f92a672a03da4104eea57f55d3076a
wichitoSP <99522188+wichitoSP@users.noreply.github.com>
2025-03-28 12:11:59 -0600

    Se crea los constructores para la clase Modelo Cita

--- src/main/java/org/example/Model/ModelCita.java
+++ src/main/java/org/example/Model/ModelCita.java
@@ -10,15 +10,65 @@ public class ModelCita {
     private  ModelVeterinario veterinario;
     private String motivo;
 
-    public ModelCita(int idCita,Date fechaHora,ModelMascota mascota,ModelDueño dueño, ModelVeterinario veterinario,String motivo){
+    public ModelCita(int idCita, Date fechaHora, ModelMascota mascota, ModelDueño dueño, ModelVeterinario veterinario, String motivo){
         this.idCita = idCita;
         this.fechaHora = fechaHora;
         this.mascota = mascota;
         this.dueño = dueño;
         this.veterinario = veterinario;
         this.motivo = motivo;
-
+        
 
     }
+
+    public int getIdCita() {
+        return idCita;
+    }
+
+    public void setIdCita(int idCita) {
+        this.idCita = idCita;
+    }
+
+    public Date getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(Date fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+    public ModelMascota getMascota() {
+        return mascota;
+    }
+
+    public void setMascota(ModelMascota mascota) {
+        this.mascota = mascota;
+    }
+
+    public ModelDueño getDueño() {
+        return dueño;
+    }
+
+    public void setDueño(ModelDueño dueño) {
+        this.dueño = dueño;
+    }
+
+    public ModelVeterinario getVeterinario() {
+        return veterinario;
+    }
+
+    public void setVeterinario(ModelVeterinario veterinario) {
+        this.veterinario = veterinario;
+    }
+
+    public String getMotivo() {
+        return motivo;
+    }
+
+    public void setMotivo(String motivo) {
+        this.motivo = motivo;
+    }
+
+
 
 }

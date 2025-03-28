36abff83127e2737255b2f4627e04b5af090ac9b
wichitoSP <99522188+wichitoSP@users.noreply.github.com>
2025-03-28 12:05:17 -0600

    Se crea los metodos Get y Set a nuestra clase Modelo Medicamento

--- src/main/java/org/example/Model/ModelMedicamento.java
+++ src/main/java/org/example/Model/ModelMedicamento.java
@@ -8,16 +8,64 @@ public class ModelMedicamento {
     private double precio;
     private int stock;
 
-    public ModelMedicamento(int id,String nombre, String descripcion, String dosis, double precio,int stock){
+    public ModelMedicamento(int id, String nombre, String descripcion, String dosis, double precio, int stock){
         this.id = id;
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.dosis = dosis;
         this.precio = precio;
         this.stock = stock;
-
-
-
+        
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDosis() {
+        return dosis;
+    }
+
+    public void setDosis(String dosis) {
+        this.dosis = dosis;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+
 
 }

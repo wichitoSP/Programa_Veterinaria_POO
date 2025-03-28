e7f6b736732db08693b6466dc48346e723edb922
wichitoSP <99522188+wichitoSP@users.noreply.github.com>
2025-03-28 11:55:54 -0600

    Se crean los Constructores get y set a la clase de Dueño

--- src/main/java/org/example/Model/ModelDueño.java
+++ src/main/java/org/example/Model/ModelDueño.java
@@ -8,7 +8,7 @@ public class ModelDueño {
     private String telefono;
     private String email;
 
-    public  ModelDueño(int id, String nombre,String apellido,String direccion,String telefono,String email){
+    public  ModelDueño(int id, String nombre, String apellido, String direccion, String telefono, String email){
         this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
@@ -18,5 +18,55 @@ public class ModelDueño {
 
 
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
 
 }

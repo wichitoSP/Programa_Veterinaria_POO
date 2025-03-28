2e75d154a194848b0a16edad420746d46aae2d41
wichitoSP <99522188+wichitoSP@users.noreply.github.com>
2025-03-28 09:15:33 -0600

    Se crea el modelo Consulta con los metodos creados

--- src/main/java/org/example/Model/ModelConsulta.java
+++ src/main/java/org/example/Model/ModelConsulta.java
@@ -12,4 +12,16 @@ public class ModelConsulta {
     private String tratamiento;
     private  double costo;
 
+    public ModelConsulta(int idConsulta, Date fecha, ModelMascota mascota, ModelVeterinario veterinario,String motivo,String diagnostico,String tratamiento,double costo){
+        this.idConsulta = idConsulta;
+        this.fecha = fecha;
+        this.mascota = mascota;
+        this.veterinario = veterinario;
+        this.motivo = motivo;
+        this.diagnostico = diagnostico;
+        this.tratamiento = tratamiento;
+        this.costo = costo;
+        
+    }
+
 }

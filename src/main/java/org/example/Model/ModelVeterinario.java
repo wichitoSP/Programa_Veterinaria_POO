c89c3936c8f56ec0a738d6868d2027313c80e271
wichitoSP <99522188+wichitoSP@users.noreply.github.com>
2025-03-28 12:00:49 -0600

    Se crea los constructores set a get a nuestra clase veterinaria

--- src/main/java/org/example/Model/ModelVeterinario.java
+++ src/main/java/org/example/Model/ModelVeterinario.java
@@ -8,7 +8,7 @@ public class ModelVeterinario {
     private String telefono;
     private String horarioTrabajo;
 
-    public  ModelVeterinario(int id, String nombre, String apellido,String especialidad,String telefono, String horarioTrabajo){
+    public  ModelVeterinario(int id, String nombre, String apellido, String especialidad, String telefono, String horarioTrabajo){
         this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
@@ -17,6 +17,58 @@ public class ModelVeterinario {
         this.horarioTrabajo = horarioTrabajo;
 
 
+
+
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getHorarioTrabajo() {
+        return horarioTrabajo;
+    }
+
+    public void setHorarioTrabajo(String horarioTrabajo) {
+        this.horarioTrabajo = horarioTrabajo;
+    }
+
+
 
 }
